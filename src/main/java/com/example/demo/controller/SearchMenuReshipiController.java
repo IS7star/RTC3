8209ef2e4a9d150fa70e0aec012d53989c8c641a
nakashima-takaharu <133740102+nakashima-takaharu@users.noreply.github.com>
2023-07-07 12:12:05 +0900

    Update SearchMenuReshipiController.java
    コントローラークラスを修正いたしましたのでアップロード致します。

--- src/main/java/com/example/demo/controller/SearchMenuReshipiController.java
+++ src/main/java/com/example/demo/controller/SearchMenuReshipiController.java
@@ -96,6 +96,32 @@ public class SearchMenuReshipiController {
 		//mvを返す
 		return mv;
 	}
+	@PostMapping("/emotionSerch")
+	public ModelAndView emoshionlist(@RequestParam("emotion") String emotion,ModelAndView mv) {
+		
+		//事前にレシピ検索していた場合、セッションの削除
+		session.removeAttribute("mood");
+		session.removeAttribute("menuInfo");
+		session.removeAttribute("menuCd");
+		session.removeAttribute("reshipiLiset");
+
+		//次画面のURLをセットする
+		mv.setViewName("menu");
+		
+		//ReshipiFormからemotion(WebAPIで取得した感情名)をreshipiFormに格納し、"emotion"キーのセッションに格納
+		ReshipiForm reshipiForm = new ReshipiForm();
+		reshipiForm.setEmotion(emotion);
+		session.setAttribute("emotion", reshipiForm);
+
+		//判定用にmoodTextに値を入れる ※nullであれば献立検索時の文章を表示
+		mv.addObject("moodText", null);
+
+		//レシピ情報を取得・セッションに格納するサービスクラスのメソッドを実行
+		searchReshipiService.getReshipiByEmotion();
+		
+		//mvを返す
+		return mv;
+	}
 	
 	/**
 	 * 取得したmenuCdをもとに対象となる献立情報詳細を表示するメソッド
