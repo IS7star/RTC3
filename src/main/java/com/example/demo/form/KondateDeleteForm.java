d159a6944bf1e31c2fdcabac06d51f0bd159d091
NiseToshiki01 <133740465+NiseToshiki01@users.noreply.github.com>
2023-07-11 10:07:24 +0900

    コメントを追加しました。 KondateDeleteForm.java

--- src/main/java/com/example/demo/form/KondateDeleteForm.java
+++ src/main/java/com/example/demo/form/KondateDeleteForm.java
@@ -3,6 +3,12 @@ package com.example.demo.form;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+/**
+ * 献立情報削除に対応するフォームです。
+ * @author sakurai
+ * @version 1.0
+ */
+
 @Data
 public class KondateDeleteForm {
 	
